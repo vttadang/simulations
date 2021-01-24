b28d4d638857f4b6cacda53de71f25e464141c04
vinhd <xuanvinh0104@yahoo.com>
2021-01-24 10:55:56 -0600

    simulate_random_variables_1.py: Output

--- simulate_random_variables_1.py
+++ simulate_random_variables_1.py
@@ -11,13 +11,13 @@ sample3_1 = np.random.uniform(0.0, 1.0, 10000)
 sample3_2 = np.random.uniform(0.0, 1.0, 10000)
 sample3_3 = sample3_1 - sample3_2
 sample3 = sample3_1 / sample3_3
-
-fig, axs = plt.subplots(3)
-axs[0].hist(sample1, bins='auto', color='b', rwidth=0.8)
-axs[0].set_title('Simulate X = -ln(U) for U ~ Unif(0,1)')
-axs[1].hist(sample2, bins='auto', color='b', rwidth=0.8)
-axs[1].set_title('Simulate Z = sqrt(-2ln(X))sin(2piY) for iid X, Y ~ Unif(0,1)')
-axs[2].hist(sample3, bins=range(-30,30), color='b', rwidth=0.8)
-axs[2].set_title('Simulate Z = X/(X - Y) for iid X, Y ~ Unif(0,1)')
-plt.subplots_adjust(hspace=0.5)
-plt.show()
+if __name__ == '__main__':
+    fig, axs = plt.subplots(3)
+    axs[0].hist(sample1, bins='auto', color='b', rwidth=0.8)
+    axs[0].set_title('Simulate X = -ln(U) for U ~ Unif(0,1)')
+    axs[1].hist(sample2, bins='auto', color='b', rwidth=0.8)
+    axs[1].set_title('Simulate Z = sqrt(-2ln(X))sin(2piY) for iid X, Y ~ Unif(0,1)')
+    axs[2].hist(sample3, bins=range(-30, 30), color='b', rwidth=0.8)
+    axs[2].set_title('Simulate Z = X/(X - Y) for iid X, Y ~ Unif(0,1)')
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
